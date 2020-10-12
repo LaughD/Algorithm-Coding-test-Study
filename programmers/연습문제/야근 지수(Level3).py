4ef76a51a6987bb89aed306a64aa7096cf101a36
LaughD <qwersky2@gmail.com>
2020-10-13 01:23:45 +0900

    Added on October 13, 2020

--- programmers/연습문제/야근 지수(Level3).py
+++ programmers/연습문제/야근 지수(Level3).py
@@ -4,10 +4,16 @@
     연습문제: 야근 지수(Level3)
     link: https://programmers.co.kr/learn/courses/30/lessons/12927
 '''
+'''
+    Edited on Oct 13, 2020
+    modified content: 0이 반환되는 조건 수정
+        before => if sum(works) < n:
+        after => if sum(works) <= n:
+'''
 import heapq
 
 def solution(n, works):
-    if sum(works) < n:
+    if sum(works) <= n:
         return 0
     heap = []
     for work in works:
