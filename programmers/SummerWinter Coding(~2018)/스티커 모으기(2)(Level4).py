8a773ef3b9042ad5721d523a6de62cbf3b41b6ec
LaughD <qwersky2@gmail.com>
2020-11-06 04:36:08 +0900

    Added on November 6, 2020

---
+++ programmers/SummerWinter Coding(~2018)/스티커 모으기(2)(Level4).py
@@ -0,0 +1,23 @@
+'''
+    Code written on Nov 6, 2020
+    출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+    Summer/Winter Coding(~2018): 스티커 모으기(2)(Level4)
+    link: https://programmers.co.kr/learn/courses/30/lessons/12971
+'''
+def solution(sticker):
+    if len(sticker) == 1:
+        return sticker.pop()
+
+    temp, sticker[1] = sticker[1], 0
+    prev = now = 0
+    for num in sticker:
+        prev, now = now, max(prev+num, now)
+    head_choice_case = prev
+
+    sticker[0], sticker[1] = 0, temp
+    prev = now = 0
+    for num in sticker:
+        prev, now = now, max(prev+num, now)
+    tail_choice_case = now
+
+    return max(head_choice_case, tail_choice_case)
