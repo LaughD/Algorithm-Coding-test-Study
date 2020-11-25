ad2eb6dae69f4bbf670a49e868e0ce26d7d48c03
LaughD <qwersky2@gmail.com>
2020-11-25 23:12:35 +0900

    Added on November 25, 2020

---
+++ programmers/SummerWinter Coding(~2018)/점프와 순간 이동(Level2).java
@@ -0,0 +1,11 @@
+/*
+    Code written on Nov 25, 2020
+    출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+    Summer/Winter Coding(~2018): 점프와 순간 이동(Level2)
+    link: https://programmers.co.kr/learn/courses/30/lessons/12980
+*/
+public class Solution {
+    public int solution(int n) {
+        return Integer.bitCount(n);
+    }
+}
