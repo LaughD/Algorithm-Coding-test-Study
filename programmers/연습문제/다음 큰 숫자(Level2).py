64ebe25659eccf8bc3fe5c8c09d92b9806145e1a
LaughD <49462445+LaughD@users.noreply.github.com>
2020-12-22 22:42:36 +0900

    Update 다음 큰 숫자(Level2).py

--- programmers/연습문제/다음 큰 숫자(Level2).py
+++ programmers/연습문제/다음 큰 숫자(Level2).py
@@ -4,6 +4,8 @@
     연습문제: 다음 큰 숫자(Level2)
     link: https://programmers.co.kr/learn/courses/30/lessons/12911
 """
+
+
 def solution(n) -> int:
     greater: int = n + (n & -n)
     insufficient: int = bin(n).count('1') - bin(greater).count('1')
