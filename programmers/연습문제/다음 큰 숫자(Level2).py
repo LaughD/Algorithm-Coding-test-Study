9db5826b51457e2c91bbe1cb37468c9dc0a56db6
LaughD <qwersky2@gmail.com>
2020-12-22 22:35:19 +0900

    Added on December 22, 2020

---
+++ programmers/연습문제/다음 큰 숫자(Level2).py
@@ -0,0 +1,12 @@
+"""
+    Code written on Dec 22, 2020
+    출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+    연습문제: 다음 큰 숫자(Level2)
+    link: https://programmers.co.kr/learn/courses/30/lessons/12911
+"""
+def solution(n) -> int:
+    greater: int = n + (n & -n)
+    insufficient: int = bin(n).count('1') - bin(greater).count('1')
+    for exponential in range(insufficient):
+        greater += (2**exponential)
+    return greater
